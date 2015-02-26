fbe583db26c50dc0b53050e981df1d4bb34a4ffa
Tyler Black <blacktylera@gmail.com>
2015-02-26 16:05:26 -0600

    win alert functionality started on row 1

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,35 +1,41 @@
+var o = '<img src="http://i.imgur.com/XeBK7pI.gif" height="100px" width="100px" class="terio">';
+    x = '<img src="https://38.media.tumblr.com/62583bc8883ed07b8e201219314b8773/tumblr_mu9v89AwEN1ssjkcoo1_400.gif" height="100px" width="100px">';
+    fb = new Firebase('https//tictacterio.firebaseio.com/');
+
+
 $(document).ready (function() {
-    var turnCount=0,
+    var turnCount = 0,
         board = [['', '', ''], ['', '', ''], ['', '', '']],
         state = board;
 
-    drawBoard(board);
-    ///this does draw a board, but the board that get drawn is not functioning as an interactive board yet//
+    //drawBoard(board);
+    ///this does draw a board, but the board that get drawn is not functioning as an interactive board yet
 
-    function drawBoard(boardArray) {
-      var $table = $('<table class="game_board table-bordered"></table>');
-      boardArray.forEach(function(row) {
-        var $row = $('<tr></tr>');
-        row.forEach(function(cell){
-          $row.append('<td>'+ cell +'</td>');
-        });
-        $table.append($row);
-      });
-      $('.container').append($table);
-    };
-    // $('#board').on('click', '.td', function(this){console.log(this)})
+    // function drawBoard(boardArray) {
+    //   var $table = $('<table class="game_board table-bordered"></table>');
+    //   boardArray.forEach(function(row) {
+    //     var $row = $('<tr></tr>');
+    //     row.forEach(function(cell){
+    //       $row.append('<td>'+ cell +'</td>');
+    //     });
+    //     $table.append($row);
+    //   });
+    //   $('.container').append($table);
+    // };
+    // // $('#board').on('click', '.td', function(this){console.log(this)})
 
     $('#board').find('td').on('click', function() {
         if(!$(this).html()) {
             if (turnCount % 2 === 0){
-                $(this).append('<img src="http://i.imgur.com/XeBK7pI.gif" height="100px" width="100px">');
+                $(this).append(x);
+                $(this).val('x');
                 didYouWin('Terio 1');
                 turnCount++;
             }else {
-                $(this).append('<img src="https://38.media.tumblr.com/62583bc8883ed07b8e201219314b8773/tumblr_mu9v89AwEN1ssjkcoo1_400.gif" height="100px" width="100px">');
-                turnCount++;
+                $(this).append(o);
+                $(this).val('o');
                 didYouWin('Terio 2');
-
+                turnCount++;
             }
         }
     });
@@ -41,86 +47,84 @@ $(document).ready (function() {
 
 
 
-    var fb = new Firebase('https//tictacterio.firebaseio.com/');
-
 
     function didYouWin(player){
-    //top row check
-        if ($('#board').find('#td1').append() !== null){
-            if ($('#board').find('#td1').append() === $('#board').find('td#2').append()) {
-                if ($('#board').find('#td1').append() === $('#td3').append()) {
-                    alert('Game over! '+player+' is the winner!');
-                    $('#newGame').removeClass('invisible');
-                }
-            }
-        }
-    //left column check
-        if ($('#board').find('#td1').append() === $('#board').find('#td4').append()) {
-            if ($('#board').find('#td1').append() === $('#td7').append()) {
-                alert('Game over! '+player+' is the winner!');
+        if ($('#board').find('#td1').val() === 'x' &&
+            $('#board').find('#td2').val() === 'x' &&
+            $('#board').find('#td3').val() === 'x'){
+                alert('OOO Kill Em! '+player+' wins!');
                 $('#newGame').removeClass('invisible');
-            }
         }
-    //left diagonal check
-        if ($('#board').find('#td1').append() === $('#board').find('#td5').append()) {
-            if ($('#board').find('#td1').append() === $('#td9').append()) {
-                alert('Game over! '+player+' is the winner!');
-                $('#newGame').removeClass('invisible');
-            }
-        }
-    //middle column check
-    if ($('#board').find('#td2').append() !== null){
-        if ($('#board').find('#td2').append() === $('#board').find('#td5').append()) {
-            if ($('#board').find('#td2').append() === $('#td8').append()) {
-                alert('Game over! '+player+' is the winner!');
-                $('#newGame').removeClass('invisible');
-            }
-        }
-    }
 
-    //right column check
-    if ($('#board').find('#td3').append() !== null){
-        if ($('#board').find('#td3').append() === $('#board').find('#td6').append()) {
-            if ($('#board').find('#td3').append() === $('#td9').append()) {
-                alert('Game over! '+player+' is the winner!');
-                $('#newGame').removeClass('invisible');
-            }
-        }
     }
-        //right diag check
-        if ($('#board').find('#td3').append() === $('#board').find('#td5').append()) {
-            if ($('#board').find('#td3').append() === $('#td7').append()) {
-                alert('Game over! '+player+' is the winner!');
-                $('#newGame').removeClass('invisible');
-            }
-        }
-    }
+    // //left column check
 
-    //middle row check
+    //     if ($('#board').find('#td1').append() === $('#board').find('#td4').append()) {
+    //         if ($('#board').find('#td1').append() === $('#td7').append()) {
+    //             alert('Game over! '+player+' is the winner!');
+    //             $('#newGame').removeClass('invisible');
+    //         }
+    //     }
+    // //left diagonal check
+    //     if ($('#board').find('#td1').append() === $('#board').find('#td5').append()) {
+    //         if ($('#board').find('#td1').append() === $('#td9').append()) {
+    //             alert('Game over! '+player+' is the winner!');
+    //             $('#newGame').removeClass('invisible');
+    //         }
+    //     }
+    // //middle column check
+    // if ($('#board').find('#td2').append() !== null){
+    //     if ($('#board').find('#td2').append() === $('#board').find('#td5').append()) {
+    //         if ($('#board').find('#td2').append() === $('#td8').append()) {
+    //             alert('Game over! '+player+' is the winner!');
+    //             $('#newGame').removeClass('invisible');
+    //         }
+    //     }
+    // }
 
-    if ($('#board').find('#td4').append() !== null){
-        if ($('#board').find('#td4').append() === $('#board').find('#td5').append()) {
-            if ($('#board').find('#td4').append() === $('#td6').append()) {
-                alert('Game over! '+player+' is the winner!');
-                $('#newGame').removeClass('invisible');
-            }
-        }
-    }
+    // //right column check
+    // if ($('#board').find('#td3').append() !== null){
+    //     if ($('#board').find('#td3').append() === $('#board').find('#td6').append()) {
+    //         if ($('#board').find('#td3').append() === $('#td9').append()) {
+    //             alert('Game over! '+player+' is the winner!');
+    //             $('#newGame').removeClass('invisible');
+    //         }
+    //     }
+    // }
+    //     //right diag check
+    //     if ($('#board').find('#td3').append() === $('#board').find('#td5').append()) {
+    //         if ($('#board').find('#td3').append() === $('#td7').append()) {
+    //             alert('Game over! '+player+' is the winner!');
+    //             $('#newGame').removeClass('invisible');
+    //         }
+    //     }
+    // }
 
-    //bottom row check
-    if ($('#board').find('#td7').append() !== null){
-        if ($('#board').find('#td7').append() === $('#board').find('#td8').append()) {
-            if ($('#board').find('#td7').append() === $('#td9').append()) {
-                alert('Game over! '+player+' is the winner!');
-                $('#newGame').removeClass('invisible');
-            }
-        }
-    }
+    // //middle row check
 
-    $('#newGame').on('click',function(){
-        $('.square').append('');
-    });
-});
+    // if ($('#board').find('#td4').append() !== null){
+    //     if ($('#board').find('#td4').append() === $('#board').find('#td5').append()) {
+    //         if ($('#board').find('#td4').append() === $('#td6').append()) {
+    //             alert('Game over! '+player+' is the winner!');
+    //             $('#newGame').removeClass('invisible');
+    //         }
+    //     }
+    // }
+
+    // //bottom row check
+    // if ($('#board').find('#td7').append() !== null){
+    //     if ($('#board').find('#td7').append() === $('#board').find('#td8').append()) {
+    //         if ($('#board').find('#td7').append() === $('#td9').append()) {
+    //             alert('Game over! '+player+' is the winner!');
+    //             $('#newGame').removeClass('invisible');
+    //         }
+    //     }
+    // }
+
+    // $('#newGame').on('click',function(){
+    //     $('.square').append('');
+    // });
+// });
 
 
 
@@ -141,4 +145,4 @@ $(document).ready (function() {
 // //                      [ 1, 5, 9 ],
 // //                      [ 7, 5, 3 ]
 // //                     ];
-
+});
